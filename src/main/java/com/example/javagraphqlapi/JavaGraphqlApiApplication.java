b883eaaf9d59764b1aa5b17c39a6b652cc0e2a1a
xiaokuan.ren <renxiaokuan9428@duia.com>
2020-04-17 09:22:01 +0800

    项目增加包

---
+++ src/main/java/com/example/javagraphqlapi/JavaGraphqlApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.javagraphqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGraphqlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGraphqlApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javagraphqlapi/JavaGraphqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javagraphqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGraphqlApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
